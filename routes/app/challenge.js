65c4f5634f8bc85966e81ae4da3b3c8e023848dd
Samuel Kitazume <samuel.kitazume@gmail.com>
2018-12-18 00:37:50 -0200

    Creating get all method, get by hash and create challenge

--- routes/app/challenge.js
+++ routes/app/challenge.js
@@ -1,9 +1,41 @@
 var express = require('express'),
+    Hashids = require('hashids'),
     challenge = express.Router();
+
+var ChallengeModel = require('./../../infra/challenge'),
+    hashids = new Hashids('challengee', 8);
+
+challenge.route('/all')
+    .get((req, res) => {
+        ChallengeModel.find({ challenger: res.locals.userId }, (err, challenges) => {
+            if (err) res.status(500).json({ message: 'Something went wront :(' });
+            res.json({ data: challenges });
+        })
+    });
+
+challenge.route('/:hash')
+    .get((req, res) => {
+        ChallengeModel.find({ hash: req.params.hash }, (err, challengeFound) => {
+            if (err) res.status(404).json({ message: 'No challenge found' });
+            res.json(challengeFound);
+        });
+    });
 
 challenge.route('/')
     .get((req, res) => {
         res.json({ message: 'You are hitting the challenge' });
+    })
+    .post((req, res) => {
+        ChallengeModel.countDocuments({}, (errCounting, count) => {
+            ChallengeModel.create({
+                challenger: res.locals.userId,
+                category: req.body.category,
+                hash: hashids.encode(count),
+                status: 0
+            }, (errCreating, newChallenge) => {
+                res.json({ message: 'New challenge created', challenge: newChallenge });
+            })
+        })
     });
 
 module.exports = challenge;
