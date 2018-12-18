b4f0ea2b2ab1c92d24391718ec9eacb78b460207
Samuel Kitazume <samuel.kitazume@gmail.com>
2018-12-18 00:37:15 -0200

    Editing routes from app path to use user data

--- routes/app/index.js
+++ routes/app/index.js
@@ -1,12 +1,17 @@
 var express = require('express'),
     app = express.Router();
 
+var UserModel = require('./../../infra/users');
 var challenge = require('./challenge');
 
-app.use('/', (req, res) => {
-    res.json({ message: 'You are hitting the root of the app' });
+app.use('/challenge', challenge);
+
+app.use('/', (req, res) => {    
+    UserModel.findById(res.locals.userId, (err, user) => {
+        if (err) res.status(500).json({ message: 'Some error occured' });
+        res.json({ message: `You are in the root of the path app, ${user.name} :)` });
+    })
 })
 
-app.use('/challenge', challenge);
 
 module.exports = app;
