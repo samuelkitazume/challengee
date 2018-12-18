afc0204c12e991191323df1f1c5077e4037b7dc4
Samuel Kitazume <samuel.kitazume@gmail.com>
2018-12-18 00:36:34 -0200

    Authentication using User Model

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,23 @@
 var express = require('express'),
+    jwt = require('jsonwebtoken'),
     auth = express.Router();
+
+var UserModel = require('./../infra/users');
 
 auth.all(['/app/','/app/*'], (req, res, next) => {
     if (!req.headers.authorization) {
         res.status(401).json({
             message: "Not authorized"
         });
-    } else next();
+    } else {
+        jwt.verify(req.headers.authorization, 'challengee', (err, token) => {
+            if (err) res.status(403).json({ message: "Invalid token" });
+            if (UserModel.findById(token.id)) {
+                res.locals.userId = token.id;
+                next();
+            } else res.status(403).json({ message: "Invalid token" });
+        });
+    }
 });
 
 module.exports = auth;
